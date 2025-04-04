7c60037502ece5af914dae0d4cafae2fcba6062f
Ray Huang <bjhuang@cs.nycu.edu.tw>
2025-04-05 03:36:20 +0800

    feat: adding event for game start/end

--- src/Knowking.sol
+++ src/Knowking.sol
@@ -8,6 +8,8 @@ contract KnowledgeKingGame {
     IERC20 public token;
     address private _owner;
     mapping(address => bool) private _userExistence;
+    uint256 private _quizIdCounter = 1; // starting from 1
+    mapping(address => uint256) private _userQuizId;
 
     constructor(IERC20 _token) {
         // specify the pre-deployed token address
@@ -29,11 +31,33 @@ contract KnowledgeKingGame {
         token.transferFrom(_owner, player, 5 * 10 ** 18);
     }
 
+    event startGame(address indexed player, uint256 quizId);
+    event endGame(address indexed player, uint256 quizId);
+
+    // call this function to start a game
     function play() external {
         require(_userExistence[msg.sender], "Player not initialized");
+        // Check if previous game is finished
+        require(_userQuizId[msg.sender] == 0, "Previous game not finished");
         // Player transfers 1 token for playing
         require(token.balanceOf(msg.sender) >= 1 * 10 ** 18, "Not enough tokens");
         token.transferFrom(msg.sender, _owner, 1 * 10 ** 18);
+        // Emit event to indicate the start of the game
+        emit startGame(msg.sender, _quizIdCounter);
+        _userQuizId[msg.sender] = _quizIdCounter;
+        // Increment quiz ID
+        _quizIdCounter++;
+    }
+
+    // call this function to end a game so that player can start a new game
+    function end() external {
+        require(_userExistence[msg.sender], "Player not initialized");
+        // Check if game is in progress
+        require(_userQuizId[msg.sender] != 0, "No game in progress");
+        // Emit event to indicate the end of the game
+        emit endGame(msg.sender, _userQuizId[msg.sender]);
+        // Reset quiz ID for player
+        _userQuizId[msg.sender] = 0;
     }
 
     function win(address player) external onlyOwner {
--- test/Knowking.t.sol
+++ test/Knowking.t.sol
@@ -55,6 +55,85 @@ contract TokenTest is Test {
         assertEq(token.balanceOf(alice), 4 * 10 ** 18, "Alice should have 4 tokens after playing");
     }
 
+    // test event startGame
+    function testStartGame() public {
+        vm.prank(owner);
+        game.initPlayer(alice);
+        assertEq(token.balanceOf(alice), 5 * 10 ** 18, "Alice should have 5 tokens");
+        vm.prank(alice);
+        token.approve(address(game), 1 * 10 ** 18);
+        vm.expectEmit(true, true, true, true);
+        emit KnowledgeKingGame.startGame(alice, 1);
+        vm.prank(alice);
+        game.play();
+    }
+
+    // test player call again play when previous game is not finished
+    function testPlayAgain() public {
+        vm.prank(owner);
+        game.initPlayer(alice);
+        assertEq(token.balanceOf(alice), 5 * 10 ** 18, "Alice should have 5 tokens");
+        vm.prank(alice);
+        token.approve(address(game), 1 * 10 ** 18);
+        vm.prank(alice);
+        game.play();
+        assertEq(token.balanceOf(alice), 4 * 10 ** 18, "Alice should have 4 tokens after playing");
+        vm.expectRevert("Previous game not finished");
+        vm.prank(alice);
+        game.play();
+    }
+
+    // test player call end to finish the game
+    function testEnd() public {
+        vm.prank(owner);
+        game.initPlayer(alice);
+        assertEq(token.balanceOf(alice), 5 * 10 ** 18, "Alice should have 5 tokens");
+        vm.prank(alice);
+        token.approve(address(game), 1 * 10 ** 18);
+        vm.prank(alice);
+        game.play();
+        assertEq(token.balanceOf(alice), 4 * 10 ** 18, "Alice should have 4 tokens after playing");
+        vm.expectEmit(true, true, true, true);
+        emit KnowledgeKingGame.endGame(alice, 1);
+        vm.prank(alice);
+        game.end();
+    }
+
+    // test player call end again when no game in progress
+    function testEndAgain() public {
+        vm.prank(owner);
+        game.initPlayer(alice);
+        assertEq(token.balanceOf(alice), 5 * 10 ** 18, "Alice should have 5 tokens");
+        vm.prank(alice);
+        token.approve(address(game), 1 * 10 ** 18);
+        vm.prank(alice);
+        game.play();
+        assertEq(token.balanceOf(alice), 4 * 10 ** 18, "Alice should have 4 tokens after playing");
+        vm.prank(alice);
+        game.end();
+        vm.expectRevert("No game in progress");
+        vm.prank(alice);
+        game.end();
+    }
+
+    // test player play again after end
+    function testPlayAfterEnd() public {
+        vm.prank(owner);
+        game.initPlayer(alice);
+        assertEq(token.balanceOf(alice), 5 * 10 ** 18, "Alice should have 5 tokens");
+        vm.prank(alice);
+        token.approve(address(game), 1 * 10 ** 18);
+        vm.prank(alice);
+        game.play();
+        assertEq(token.balanceOf(alice), 4 * 10 ** 18, "Alice should have 4 tokens after playing");
+        vm.prank(alice);
+        game.end();
+        vm.prank(alice);
+        token.approve(address(game), 1 * 10 ** 18);
+        vm.prank(alice);
+        game.play();
+    }
+
     // test Player win
     function testWin() public {
         vm.prank(owner);
